a14df060e79dcbe407c1a6bddaf583b26eec1a7a
Sinimawath <sinimawath@gmail.com>
2018-04-21 13:01:57 +0300

    Added a few objects & chagne gameinstance

--- GameObjects/gameinstance.h
+++ GameObjects/gameinstance.h
@@ -6,12 +6,16 @@
 #include <vector>
 #include <memory>
 #include <GameViews/gamescene.h>
+#include <GameObjectsViews/tankview.h>
+#include <GameObjectsViews/bulletview.h>
 #include "GameObjects/gamephysicsengineimpl.h"
+
 class GameInstance: public QWidget
 {
 public:
     GameInstance();
-
+    void renderInterface();
+    void startGame();
 private:
     // ClientData
 
@@ -19,7 +23,13 @@ private:
     std::unique_ptr<QTimer> gameTimer_;
     std::unique_ptr<QTimer> netTimer_;
 
+    //QObject use row pointers
+    TankView* playerTankView_;
+
+    std::vector<TankView*> allTanks_;
+    std::vector<BulletView*> allBullets_;
     std::shared_ptr<GamePhysicsEngine> gamePhysicsEngine_;
+
     //b2World b2GameWorld_;
     //std::vector<Tank*> tanks_;
 
--- GameObjectsViews/wallview.cpp
+++ GameObjectsViews/wallview.cpp
@@ -1,6 +1,21 @@
 #include "wallview.h"
 
-WallView::WallView()
+WallView::WallView(Wall *wall): wall_(wall)
+{
+
+}
+
+WallView::~WallView()
+{
+
+}
+
+QRectF WallView::boundingRect() const
+{
+
+}
+
+void WallView::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
 
 }
